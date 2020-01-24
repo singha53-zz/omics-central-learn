11f9dd79c9fcd56ade5d256b692eb905de5531bb
Amrit Singh <asingh.analytics@gmail.com>
2020-01-24 16:43:42 -0500

    add customTheme() to helper_functions.R

--- helper_functions.R
+++ helper_functions.R
@@ -7,7 +7,34 @@
 
 # load libraries
 library(tidyverse);
-library(amritr);
+
+################################################################################
+#
+# improve ggplot axes aesthetics
+# Description: improve axes aesthetics
+# Inputs:      sizeStripFont: size of facet label,
+#              xAngle: angle of x-axis label,
+#              hjust: horizontal alignement of x-axis label,
+#              vjust: vertical alignement of x-axis label,
+#              xSize: size of x-axis labels,
+#              ySize: size of y-axis labels,
+#              xAxisSize: size of x-axis heading,
+#              yAxisSize: size of y-axis heading
+# Outputs:     layer to add to a ggplot
+#
+################################################################################
+customTheme = function (sizeStripFont = 10,
+  xAngle = 0, hjust = 0.5, vjust = 0.5, xSize = 10, ySize = 10,
+  xAxisSize = 10, yAxisSize = 10) {
+  theme(strip.background = element_rect(colour = "black", fill = "white",
+    size = 1), strip.text.x = element_text(size = sizeStripFont),
+    strip.text.y = element_text(size = sizeStripFont), axis.text.x = element_text(angle = xAngle,
+      hjust = hjust, vjust = vjust, size = xSize, color = "black"),
+    axis.text.y = element_text(size = ySize, color = "black"),
+    axis.title.x = element_text(size = xAxisSize, color = "black"),
+    axis.title.y = element_text(size = yAxisSize, color = "black"),
+    panel.background = element_rect(fill = "white", color = "black"))
+}
 
 ################################################################################
 #
